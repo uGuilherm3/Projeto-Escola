754bb01995f15fb8db77d5c69b92c7275c0b7399
Guilherme Pontes <guilhermea.pontes@outlook.com>
2022-04-30 11:09:43 -0300

    patch

--- script.js
+++ script.js
@@ -4,17 +4,6 @@ btn.addEventListener('click', () => {
     window.scroll({top: window.innerHeight, behavior: "smooth"})
 })
 
-//var btn = document.querySelector('.click');
-//var imagem = document.querySelector('.img');
-//var imagem2 = document.querySelector('.img2')
-
-//btn.addEventListener('click', function(){
-//    if (imagem.style.display, imagem2.style.display === "block") {
-//        (imagem.style.display, imagem2.style.display = 'none');
-//    }else{
-//        imagem.style.display, imagem2.style.display = 'block';
-//    }
-//})
 var tempo = 3000,
     primeira = 0,
     images = document.querySelectorAll(".slides img")
@@ -34,4 +23,5 @@ var tempo = 3000,
         }, tempo)
     }
 
-    window.addEventListener("load", inicio)  
+    window.addEventListener("load", inicio) 
+    
