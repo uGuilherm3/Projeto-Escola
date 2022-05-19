fe3935475d5bca60a9b4b4acb2238b130c44693d
Guilherme Pontes <guilhermea.pontes@outlook.com>
2022-05-19 03:21:06 -0300

    patch

--- script.js
+++ script.js
@@ -11,26 +11,7 @@ btn2.addEventListener('click', () => {
 })
 
 //slide
-var tempo = 2000,
-    primeira = 0,
-    span = document.querySelectorAll(".mencoes span")
-    max = span.length;
-
-    function proxima() {
-        span[primeira].classList.remove("selecionado")
-        primeira++
-        if(primeira >= max)
-        primeira = 0
-        span[primeira].classList.add("selecionado")
-    }
-
-    function inicio() {
-        setInterval(() => {
-            proxima()
-        }, tempo)
-    }
-
-    window.addEventListener("load", inicio) 
+ 
 //slide-end
 
 const btn3 = document.querySelector('.topo')
