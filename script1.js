4a1b894a59604099a2b93daa33bbc5a161d7afc5
Guilherme Pontes <guilhermea.pontes@outlook.com>
2022-05-25 15:21:58 -0300

    patch

--- script1.js
+++ script1.js
@@ -1,5 +1,18 @@
-const btn = document.querySelector('.btn')
 
-btn.addEventListener('click', () => {
-    window.scroll({top: window.innerHeight*0.8, behavior: "smooth"})
+const target = document.querySelectorAll('[data-anime]');
+const animate = 'anima';
+
+function rolar() {
+    const topo = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+        if((topo) > element.offsetTop) {
+            element.classList.add('anima')
+        } else {
+            element.classList.remove('anima')
+        }
+    })
+}
+
+window.addEventListener('scroll', function() {
+    rolar();
 })
